2ac03b72c7088e137fa894b03669c820ab16cdb7
Amalguittoum <fa_guittoum@esi.dz>
2019-07-31 15:28:34 +0100

    get services

---
+++ services.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var mysql = require("mysql");
+var app = express();
+
+//database connection
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'phpmyadmin',
+    password : 'root',
+    database:'PharmaDz'
+});
+connection.connect();
+
+
+// server creation
+
+var server = app.listen(8082,function(){
+    var host = server.address().address
+    var port = server.address().port
+});
+
+// rest service
+app.get('/getpharmacies',function(req,res){  
+    var query = "select * from Pharmacie";
+   connection.query(query,function(error,results){
+    if (error) throw error;
+    res.send(JSON.stringify(results));
+})
+});
+app.get('/getcommandes',function(req,res){  
+    var query = "select * from Commande";
+   connection.query(query,function(error,results){
+    if (error) throw error;
+    res.send(JSON.stringify(results));
+})
+});
