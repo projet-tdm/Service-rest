58cdfd71d02de3be84699c602b0c5fff9ddba960
Amalguittoum <fa_guittoum@esi.dz>
2019-08-09 01:01:05 +0100

    update service

--- services.js
+++ services.js
@@ -20,9 +20,9 @@ var server = app.listen(8083,function(){
 });
 
 // rest service
-app.get('/getpharmacies',function(req,res){  
-    var query = "select * from Pharmacie";
-   connection.query(query,function(error,results){
+app.get('/getpharmacies/:ville',function(req,res){  
+    var query = "select * from Pharmacie where id_ville=?";
+   connection.query(query,[req.params.ville],function(error,results){
     if (error) throw error;
     res.send(JSON.stringify(results));
 })
@@ -34,3 +34,18 @@ app.get('/getcommandes',function(req,res){
     res.send(JSON.stringify(results));
 })
 });
+app.get('/getvilles',function(req,res){  
+    var query = "select * from Ville";
+   connection.query(query,function(error,results){
+    if (error) throw error;
+    res.send(JSON.stringify(results));
+})
+});
+app.put('/annuler/:id', function (req, res) {
+    var query="UPDATE Commande SET etat='A' where numero=?";
+    connection.query(query,[req.params.id],function(error,results){
+        if (error) throw error;
+        res.send(JSON.stringify(results));
+    })
+ });
+ 
